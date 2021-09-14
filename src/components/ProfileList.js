98ac1eb813e3914966ebe1554438c9f87d798c61
usernamebuzz <ohminji@ewhain.net>
2021-09-14 12:47:36 +0900

    array.map w/ <Fragment>

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -1,7 +1,41 @@
 import styled from "styled-components";
 import ProfileCard from "./ProfileCard";
+import React, {Fragment} from 'react';
 
-// hint: Array.map, 반복되는 코드를 하나의 컴포넌트(card)를 만들어서 재사용
+const languages=[
+  {name: "Python", age: "30", type: "object-oriented", 
+    inheritance: "provides both single and multiple inheritance",
+    thread: "Supports multithreading",
+    library: "Has a huge set of libraries that make it fit for AI, datascience, etc.",
+    website: "www.python.org"},
+
+  {name: "C", age: "49", type: "imperative procedural", 
+    inheritance: "provide partial multiple inheritance using interfaces",
+    thread: "Does Not support threads",
+    library: "Has limited number of library support",
+    website: "www.iso.org/standard/74528.html"},
+
+  {name: "Java", age: "26", type: "object-oriented", 
+    inheritance: "provides both single and multiple inheritance",
+    thread: "Has in build multithreading support",
+    library: "Has library support for many concepts like UI",
+    website: "oracle.com/java/"},
+];
+
 export default function ProfileList() {
-  return <div></div>;
-}
+  return <div>
+    <h1>Computer Languages</h1>
+        {languages.map((language, index) =>(
+          <Fragment>
+          <h3 key={index}>{language.name}</h3>
+          <p>
+            <li>{language.age} years old</li>
+            <li>{language.type}</li>
+            <li>{language.inheritance}</li>
+            <li>{language.thread}</li>
+            <li>{language.library}</li><br/>
+            For further information, visit {language.website}
+          </p>
+          </Fragment>
+          ))}
+  </div>;} 
