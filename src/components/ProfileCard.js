d1730992ed8cf1e2a5a979052777e85d960f5aa2
yongyeonkim-dev <seyeon.dev@gmail.com>
2021-09-14 21:26:44 +0900

    feat: Render first ProfileCard

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,10 +1,28 @@
 import { useState } from "react";
 import styled from "styled-components";
 
-export default function ProfileCard() {
+export default function ProfileCard({name, major, currentAge, nextYearAge, movies}) {
   const [likes, setLikes] = useState(0); // 좋아요 개수
 
-  return <Wrapper></Wrapper>;
+  return <Wrapper>
+    <div className="profile__likes">
+      <button>좋아요</button>
+      <span> {likes}명이 좋아요를 눌렀습니다</span>
+    </div>
+    <div className="profile__data">
+      <h2>{name}</h2>
+      <p>안녕하세요? 저는 이화여자대학교 {major}에 다니고 있어요. <br />
+        올해는 {currentAge}살이고, 내년에는 {nextYearAge}살이에요.
+      </p>
+      <h3>좋아하는 영화</h3>
+      <ul>
+        {movies.map((movie) => (
+          <li>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  </Wrapper>;
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+`;
--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -2,6 +2,11 @@ import styled from "styled-components";
 import ProfileCard from "./ProfileCard";
 
 // hint: Array.map, 반복되는 코드를 하나의 컴포넌트(card)를 만들어서 재사용
+const movies=["대니쉬 걸", "포레스트 검프"]
+
 export default function ProfileList() {
-  return <div></div>;
+  console.log(movies)
+  return <div>
+    <ProfileCard name="김용연" major="컴퓨터공학과" currentAge="21" nextYearAge="22" movies={movies}/>
+  </div>;
 }
