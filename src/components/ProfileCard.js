8857fd1ee054137fd7f7a344546378e5a67a7d25
Sojin Lee <77518932+SJ-Lee33@users.noreply.github.com>
2021-09-17 22:00:47 +0900

    modify line-break

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,24 +1,19 @@
 import { useState } from "react";
 import styled from "styled-components";
-import { GoThumbsup, GoThumbsdown } from 'react-icons/go';
+import { GoThumbsup, GoThumbsdown } from "react-icons/go";
 
 export default function ProfileCard({ candidates }) {
-  
   // 좋아요 추가
-  const [likes, setLikes] = useState(0); 
+  const [likes, setLikes] = useState(0);
   const onIncrease = () => {
-    setLikes(prevLikes => prevLikes + 1);
-  }  
+    setLikes((prevLikes) => prevLikes + 1);
+  };
 
   // 소개말 줄바꿈
-  const information = candidates.info.split('\n').map(
-    data => (<span>{data}<br /></span>)
-  );
+  const information = candidates.info.map((data) => <p>{data}</p>);
 
   // 영화 리스트
-  const movies = candidates.movie.map(
-    (mov) => (<li>{mov}</li>)
-  );
+  const movies = candidates.movie.map((mov) => <li>{mov}</li>);
 
   // 싫어요
   const onDecrease = () => {
@@ -28,16 +23,18 @@ export default function ProfileCard({ candidates }) {
   return (
     <Wrapper>
       <Like>
-        <Button onClick={onIncrease}><GoThumbsup /></Button>
+        <Button onClick={onIncrease}>
+          <GoThumbsup />
+        </Button>
         <span>{likes}명이 좋아요를 눌렀습니다.</span>
       </Like>
       <h1>{candidates.name}</h1>
-      <span >
-        {information}
-      </span>
+      <span>{information}</span>
       <h2>좋아하는 영화</h2>
       <ul>{movies}</ul>
-      <Button color="blue" onClick={onDecrease}><GoThumbsdown /></Button>
+      <Button color="blue" onClick={onDecrease}>
+        <GoThumbsdown />
+      </Button>
     </Wrapper>
   );
 }
@@ -48,7 +45,7 @@ const Wrapper = styled.div`
   border-radius: 5rem;
   padding: 3rem;
   margin: 5rem;
-  box-shadow: 3px 3px 10px rgba(0,0,0,0.3);
+  box-shadow: 3px 3px 10px rgba(0, 0, 0, 0.3);
 
   &:hover {
     background-color: #b2f2bb;
@@ -56,7 +53,7 @@ const Wrapper = styled.div`
 `;
 
 const Like = styled.div`
-  float:right;
+  float: right;
 `;
 
 const Button = styled.button`
@@ -65,12 +62,12 @@ const Button = styled.button`
   border: none;
   border-radius: 50%;
   color: white;
-  box-shadow: 1px 1px 1px rgba(0,0,0,0.5);
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, 0.5);
   cursor: pointer;
   padding: 0.5rem;
   margin-right: 0.8rem;
 
-  background-color: ${props => props.color || "salmon"};
+  background-color: ${(props) => props.color || "salmon"};
   &:hover {
     color: yellow;
   }
