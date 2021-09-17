bd3d0d2a400890bdc5dfbc5dec701e15ab7df197
Sojin Lee <77518932+SJ-Lee33@users.noreply.github.com>
2021-09-17 19:57:08 +0900

    context change

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -6,27 +6,29 @@ export default function ProfileList() {
   const candidates = [
     {
       id: 1,
-      name: '이소진',
+      name: "LSJ",
       info: `안녕하세요? 저는 이화여자대학교 컴퓨터공학과 19학번이에요.\n주민번호 뒷자리 4인 밀레니엄 베이비!`,
-      movie: ['블랙위도우','마당을 나온 암탉'],
+      movie: ["블랙위도우", "마당을 나온 암탉"],
     },
     {
       id: 2,
-      name: '김이화',
-      info: '안녕하세요? 저는 이화여자대학교 실용음학과 19학번이에요.\n노래는 별로 안좋아해요.',
-      movie: ['라이온 킹', '파바로티'],
+      name: "김이화",
+      info: "안녕하세요? 저는 이화여자대학교 실용음악과 19학번이에요.\n노래는 별로 안좋아해요.",
+      movie: ["라이온 킹", "파바로티"],
     },
     {
       id: 3,
-      name: '닌자',
-      info: '안녕하세요? 저는 닌자예요.\n미안하지만 정체는 비밀이에요.',
-      movie: ['보스베이비', '나루토질풍전','꾸러기 닌자 토리'],
+      name: "닌자",
+      info: "안녕하세요? 저는 닌자예요.\n미안하지만 정체는 비밀이에요.",
+      movie: ["보스베이비", "나루토질풍전", "꾸러기 닌자 토리"],
     },
-  ]
+  ];
 
-  return <div>
-    {candidates.map(candidates => (
-      <ProfileCard candidates = {candidates} key={candidates.id} />
-    ))}
-  </div>;
+  return (
+    <div>
+      {candidates.map((candidates) => (
+        <ProfileCard candidates={candidates} key={candidates.id} />
+      ))}
+    </div>
+  );
 }
