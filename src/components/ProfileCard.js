d31a713e9f052ae7677310cb3cb912b802215f53
songing01 <79829662+songing01@users.noreply.github.com>
2021-09-17 18:10:08 +0900

    create profile card

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,10 +1,32 @@
 import { useState } from "react";
 import styled from "styled-components";
 
-export default function ProfileCard() {
+export default function ProfileCard(candidates) {
   const [likes, setLikes] = useState(0); // 좋아요 개수
+  const add_like = () => {
+    setLikes(likes + 1);
+  };
 
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <Like>
+        <button onClick={add_like}>좋아요</button>
+        {likes}명이 좋아요를 눌렀습니다.
+      </Like>
+      <Profile>
+        <header>{candidates.name}</header>
+        안녕하세요? 저는 이화여자대학교 {candidates.major}에 다니고 있어요.
+        올해는 {candidates.age}
+        살이고, 내녀에는{candidates.age + 1}살이에요.
+      </Profile>
+    </Wrapper>
+  );
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  background-color: white;
+  width: 60%;
+  outline: solid;
+`;
+const Like = styled.div``;
+const Profile = styled.div``;
--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -2,6 +2,29 @@ import styled from "styled-components";
 import ProfileCard from "./ProfileCard";
 
 // hint: Array.map, 반복되는 코드를 하나의 컴포넌트(card)를 만들어서 재사용
+const candidates = [
+  {
+    name: "송지민",
+    major: "컴퓨터공학과",
+    age: 21,
+  },
+  {
+    name: "뽀로로",
+    major: "컴퓨터공학과",
+    age: 21,
+  },
+];
 export default function ProfileList() {
-  return <div></div>;
+  return (
+    <div>
+      {candidates.map((candidate, index) => (
+        <ProfileCard
+          key={index}
+          name={candidate.name}
+          major={candidate.major}
+          age={candidate.age}
+        />
+      ))}
+    </div>
+  );
 }
