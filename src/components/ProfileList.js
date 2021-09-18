f27b8753ad5ec10bb35657d957831766e95eb199
yongyeonkim-dev <seyeon.dev@gmail.com>
2021-09-18 10:01:50 +0900

    Remove duplicated age
    nextYearAge 삭제하고 현재 나이에 +1

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -6,24 +6,21 @@ const USERS = [
     id: 0,
     name: "김용연",
     major: "컴퓨터공학과",
-    currentAge: 21,
-    nextYearAge: 22,
+    age: 21,
     movies: ["대니쉬 걸", "포레스트 검프"],
   },
   {
     id: 1,
     name: "김영희",
     major: "조경학과",
-    currentAge: 20,
-    nextYearAge: 21,
+    age: 20,
     movies: ["포미니츠", "빌리 엘리어트"],
   },
   {
     id: 2,
     name: "권철수",
     major: "경영학과",
-    currentAge: 22,
-    nextYearAge: 23,
+    age: 22,
     movies: ["샹치", "너의 이름은"],
   },
 ];
@@ -36,8 +33,8 @@ export default function ProfileList() {
           key={USERS.id}
           name={USERS.name}
           major={USERS.major}
-          currentAge={USERS.currentAge}
-          nextYearAge={USERS.nextYearAge}
+          age={USERS.age}
+          nextYearAge={USERS.age + 1}
           movies={USERS.movies}
         />
       ))}
