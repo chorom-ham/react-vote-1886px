c0f2f979af62d14f2cc4fa409eea307bb2f5e984
Yeonhee_Jung <spig0126@gmail.com>
2021-09-18 19:50:48 +0900

    feat : create profiles and add ProfileCard for each profile

--- src/components/ProfileList.js
+++ src/components/ProfileList.js
@@ -1,7 +1,42 @@
 import styled from "styled-components";
+import {GlobalStyle} from "../App"
 import ProfileCard from "./ProfileCard";
+import {useState} from "react";
+import React from "react";
 
 // hint: Array.map, 반복되는 코드를 하나의 컴포넌트(card)를 만들어서 재사용
 export default function ProfileList() {
-  return <div></div>;
-}
+    const schoolDefault = useState("이화여자대학교");
+    const majorDefault = useState("컴퓨터공학전공");
+    const people = [
+        {
+            name: "정연희",
+            school: schoolDefault,
+            major: majorDefault,
+            age: 21,
+            movies: ["헝거게임", "다이버전트", "러브 로지"]
+        },
+        {
+            name: "김유민",
+            school: schoolDefault,
+            major: majorDefault,
+            age: 21,
+            movies: ["인터스텔라", "그래비티"]
+        },
+        {
+            name: "김용연",
+            school: schoolDefault,
+            major: majorDefault,
+            age: 21,
+            movies: ["대뉘시 걸", "포레스트 검프"]
+        }
+    ]
+
+    return (
+        <div>
+            {people.map((person, index) =>
+                <ProfileCard person={person} />
+            )}
+        </div>
+    );
+};
