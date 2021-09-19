ae76dd52566ff37e23b382922a175cc7b6635081
Yeonhee_Jung <spig0126@gmail.com>
2021-09-20 01:26:05 +0900

    feat : add unlike function(LikeButton) and change css(StyledButtonSection)

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -3,11 +3,6 @@ import styled, {css} from "styled-components";
 import {darken, lighten} from "polished";
 
 export default function ProfileCard({person}) {
-    const [likes, setLikes] = useState(0); // 좋아요 개수
-    const like = () => (
-        setLikes(likes + 1)
-    );
-
     const MovieList = () => (
         <StyledMovieList>
             <h3>좋아하는 영화</h3>
@@ -18,17 +13,42 @@ export default function ProfileCard({person}) {
     );
 
 
-    const ButtonSection = () => (
-        <StyledButtonSection>
-            <button value='unlike' onClick={like}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                    <path
-                        d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"/>
-                </svg>
-            </button>
-            <p>{likes}명이 좋아요를 눌렀습니다</p>
-        </StyledButtonSection>
-    );
+    function ButtonSection() {
+        const [likes, setLikes] = useState(0); // 좋아요 개수
+        const [likeState, setLikeState] = useState("unlike");
+        const [color, setColor] = useState("grey");
+        const like = () => {
+            setLikes(likes + 1)
+            setColor("Red")
+        };
+        const unlike = () => {
+            setLikes(likes - 1)
+            setColor("grey")
+        };
+
+        const LikeButton = () => {
+            if (likeState === "unlike") {
+                like();
+                setLikeState("like");
+            } else {
+                unlike();
+                setLikeState("unlike");
+            }
+        };
+
+        return (
+            <StyledButtonSection color={color}>
+                <button value='unlike' onClick={LikeButton}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path
+                            d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"/>
+                    </svg>
+                </button>
+                <p>{likes}명이 좋아요를 눌렀습니다</p>
+            </StyledButtonSection>
+        );
+    };
+
 
     return (
         <Wrapper>
@@ -66,28 +86,35 @@ const Wrapper = styled.div`
 `;
 
 const StyledButtonSection = styled.div`
-    display: inline-flex;
-    justify-content: flex-end;
+  display: inline-flex;
+  justify-content: flex-end;
+  
+  p {
+    font-size: 1.1rem;
+    margin: 0;
+    color: grey;
+  }
+  
+  ${ ({color}) => {
+      return css`
+        button {
+          fill: ${color};
+          border: none;
+          background-color: transparent;
+          transition-duration: 0.1s;
+        }
+    
+        button:hover {
+          fill: ${darken(0.2, color)};
+        }
+    `;
+  }}
 
-    p {
-        font-size: 1.1rem;
-        margin: 0;
-        font-color: grey;
-    }
-    button {
-        fill:red;
-        border: none;
-        background-color: transparent;
-        transition-duration: 0.2s ;
-    }
-    button:hover{
-        fill: ${darken(0.1, "Red")};
-    }
-    svg {
-        margin-top: 0.4rem;
-        width: 1.8rem;
-        height: 1.8rem;
-    }
+  svg {
+    margin-top: 0.4rem;
+    width: 1.8rem;
+    height: 1.8rem;
+  }
 `;
 
 const StyledMovieList = styled.div`
