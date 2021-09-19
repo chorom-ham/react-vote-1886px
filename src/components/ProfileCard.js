f332bf32028a95f2a529573134ebcff77d451075
Yeonhee_Jung <spig0126@gmail.com>
2021-09-20 00:11:13 +0900

    docs : changed button related HTML elements into component ButtonSection

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -8,27 +8,38 @@ export default function ProfileCard({person}) {
         setLikes(likes + 1)
     );
 
-    const MovieList = person.movies.map((movie, index) =>
-        <li key = {index}>{movie}</li>
+    const MovieList = () => (
+        <StyledMovieList>
+            <h3>좋아하는 영화</h3>
+            <ul>
+                {person.movies.map((movie, index) => <li key={index}>{movie}</li>)}
+            </ul>
+        </StyledMovieList>
+    );
+
+
+    const ButtonSection = () => (
+        <StyledButtonSection>
+            <button value='unlike' onClick={like}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path
+                        d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"/>
+                </svg>
+            </button>
+            <p>{likes}명이 좋아요를 눌렀습니다</p>
+        </StyledButtonSection>
     );
 
     return (
         <Wrapper>
-            <div className="buttonSection">
-                <button value='unlike' onClick={like}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.435c-1.989-5.399-12-4.597-12 3.568 0 4.068 3.06 9.481 12 14.997 8.94-5.516 12-10.929 12-14.997 0-8.118-10-8.999-12-3.568z"/></svg>
-                </button>
-                <p>{likes}명이 좋아요를 눌렀습니다</p>
-            </div>
+            <ButtonSection />
             <h2>{person.name}</h2>
             <p>
                 안녕하세요! 저는 {person.school} {person.major}에 다니고 있어요.
                 올해는 {person.age}살이고, 내년에는 {person.age+1}살이에요.
             </p>
-            <h3>좋아하는 영화</h3>
-            <ul>
-                {MovieList}
-            </ul>
+            <MovieList />
+
         </Wrapper>
     );
 };
@@ -42,33 +53,50 @@ const Wrapper = styled.div`
   background-color: white;
   border-radius: 1.5rem;
   
-  ul {
+  h2{
+    font-size: 3rem;
+    font-weight: bolder;
     margin: 0;
   }
-  li {
-    font-size: 1.3rem;
+  p{
+    font-size: 1.4rem;
+    line-height: 2.5rem;
+    margin: 1.5rem 0;
   }
-  .buttonSection {
+`;
+
+const StyledButtonSection = styled.div`
     display: inline-flex;
     justify-content: flex-end;
-  }
-  .buttonSection > p {
-    font-size: 1.1rem;
-    margin: 0;
-    font-color: grey;
-  }
-  button {
-    fill:red;
-    border: none;
-    background-color: transparent;
-    transition-duration: 0.2s ;
-  }
-  button:hover{
-    fill: ${darken(0.1, "Red")};
-  }
-  svg {
-    margin-top: 0.4rem;
-    width: 1.8rem;
-    height: 1.8rem;
-  }
+
+    p {
+        font-size: 1.1rem;
+        margin: 0;
+        font-color: grey;
+    }
+    button {
+        fill:red;
+        border: none;
+        background-color: transparent;
+        transition-duration: 0.2s ;
+    }
+    button:hover{
+        fill: ${darken(0.1, "Red")};
+    }
+    svg {
+        margin-top: 0.4rem;
+        width: 1.8rem;
+        height: 1.8rem;
+    }
+`;
+
+const StyledMovieList = styled.div`
+    li {
+    font-size: 1.3rem;
+    }
+    h3{
+    font-size: 2rem;
+    margin-bottom: 0.8rem;
+    margin-top: 0rem;
+    }
 `;
